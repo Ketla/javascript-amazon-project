60805a75ae2d818fa63e7f9653f485a78674a45d
Boomzpwnz <kjetil.wasteson@gmail.com>
2023-10-12 12:57:36 +0200

    Quanity selector now working

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -25,7 +25,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -64,6 +64,10 @@ document.querySelectorAll('.js-add-to-cart')
 
       let matchingItem;
 
+      const itemQuantity = document.querySelector(`.js-quantity-selector-${productId}`);
+
+      const selectedQuantity = Number(itemQuantity.value);
+
       cart.forEach((item) => {
         if (productId  === item.productId) {
           matchingItem = item;
@@ -71,11 +75,11 @@ document.querySelectorAll('.js-add-to-cart')
       });
 
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += selectedQuantity;
       } else {
         cart.push({
           productId : productId ,
-          quantity: 1
+          quantity: selectedQuantity
         })
       }
 
@@ -89,5 +93,9 @@ document.querySelectorAll('.js-add-to-cart')
 
       console.log(cartQuantity);
       console.log(cart);
+
+
+
+
     });
   })
